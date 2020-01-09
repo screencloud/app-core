0f28b38ce27db9eb94b63fb25a777c9c4a127570
Macavirus <macavirus@zoho.com>
2020-01-09 19:24:13 +0700

    fix(eslint): add prettier to eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         es6: true,
     },
     parser: '@typescript-eslint/parser',
-    extends: ['plugin:@typescript-eslint/recommended'],
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
     parserOptions: {
         ecmaVersion: 2018,
         sourceType: 'module',
