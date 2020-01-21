c5e79609ffb775ef0527d291064c548e7e3f19e7
Macavirus <macavirus@zoho.com>
2020-01-21 13:16:05 +0700

    fix(PostMessageBridge): add more valid domains

--- src/PostMessageBridge.ts
+++ src/PostMessageBridge.ts
@@ -28,15 +28,24 @@ export function encodePostMessageBridgeCommand(type: PostMessageBridgeCommandTyp
 }
 
 export class PostMessageBridge extends Bridge {
-
+    // Note: terminating "/" or port number is mandatory
     private static validPostMessageDomains = [
         "https://apps-services.screencloudapp.com/",
         "https://apps-backends-production.screen.cloud/",
         "https://apps-services.staging.screencloudapp.com/",
         "https://apps-backends-staging.screen.cloud/",
-        "https://studio.staging.next.sc/",
+        "https://screencloud-testing.auth0.com/",
+        "https://authenticate.next.sc/",
+        "https://studio.dev.next.sc:3000",
+        "https://studio.dev.next.sc/",
         "https://studio.edge.next.sc/",
-        "https://studio.screencloud.com/"
+        "https://studio.staging.next.sc/",
+        "https://studio.screencloud.com/",
+        "https://mc.dev.next.sc:3001",
+        "https://mc.dev.next.sc/",
+        "https://mc.edge.next.sc/",
+        "https://mc.staging.next.sc/",
+        "https://mc.screencloud.com/"
     ];
 
     protected targetWindow: Window | null = null;
