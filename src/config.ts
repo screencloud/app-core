cc4939e947d99412addd7daf995ad9034dcf5ff8
Macavirus <macavirus@zoho.com>
2020-01-22 18:57:11 +0700

    fix: add config file with validMessageDomains

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+export const validMessageDomains = [
+    // Note: terminating "/" or port number is mandatory
+    "https://apps-services.screencloudapp.com/",
+    "https://apps-backends-production.screen.cloud/",
+    "https://apps-services.staging.screencloudapp.com/",
+    "https://apps-backends-staging.screen.cloud/",
+    "https://screencloud-testing.auth0.com/",
+    "https://authenticate.next.sc/",
+    "https://studio.dev.next.sc:3000",
+    "https://studio.dev.next.sc/",
+    "https://studio.edge.next.sc/",
+    "https://studio.staging.next.sc/",
+    "https://studio.screencloud.com/",
+    "https://mc.dev.next.sc:3001",
+    "https://mc.dev.next.sc/",
+    "https://mc.edge.next.sc/",
+    "https://mc.staging.next.sc/",
+    "https://mc.screencloud.com/"
+];
